dca1a320d2f0a864bd545ba0b1b5fc4f0c72a6fa
syedabdulqadirgilani <54541129+syedabdulqadirgilani@users.noreply.github.com>
2020-05-01 04:58:40 +0500

    Delete case insensitive.py

--- case insensitive.py
+++
@@ -1,3 +0,0 @@
-user_name,single_character=input('enter user name and a single character comma seperated ').split(',')
-print(f'length of name is:- {len(user_name)}')
-print(f'character count {user_name.strip().lower().count(single_character.strip().lower())}')
