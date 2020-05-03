f93b024213d9562030b6e37ab322627389f8b8fd
syedabdulqadirgilani <54541129+syedabdulqadirgilani@users.noreply.github.com>
2020-05-04 01:36:28 +0500

    Update number guessing game (nested ifelse).py

--- number guessing game (nested ifelse).py
+++ number guessing game (nested ifelse).py
@@ -3,7 +3,7 @@ u_n=int(input('enter '))
 if u_n==w_n:
     print('you win')
 else:
-if u_n<w_n:
-    print('too low!')
+    if u_n<w_n:
+        print('too low!')
 if u_n>w_n:
     print('too high!')
